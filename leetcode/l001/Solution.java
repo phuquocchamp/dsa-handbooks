135b6706f2f2d3b17d47a543b3494702144785eb
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-09-28 16:22:48 +0700

    upload l001 solution

--- leetcode/l001/Solution.java
+++ leetcode/l001/Solution.java
@@ -1,4 +1,32 @@
 package l001;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+
+        if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two sum solution found.");
+        }
+    }
 }
