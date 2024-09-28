838de7e00b72fc01cd663e69cf9d7ac51ef5ba9d
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-09-28 16:23:13 +0700

    create l003 problem

---
+++ leetcode/l003/Solution.java
@@ -0,0 +1,12 @@
+package l003;
+
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0.1;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
