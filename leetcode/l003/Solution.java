9ce36809337323380d41cfaf17dc0f6239c6f890
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-10-22 21:51:54 +0700

    create README.MD file

--- leetcode/l003/Solution.java
+++ leetcode/l003/Solution.java
@@ -1,12 +1,31 @@
 package l003;
+import java.util.HashSet;
 
-public class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return 0.1;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> container = new HashSet<>();
+        int left = 0, result = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            if(!container.contains(s.charAt(right))){
+                container.add(s.charAt(right));
+                result = Math.max(result, right - left + 1);
+            }else{
+                while(container.contains(s.charAt(right))){
+                    container.remove(s.charAt(left));
+                    left++;
+                }
+                container.add(s.charAt(right));
+            }
+        }
+        
+        return result;
     }
 
-
     public static void main(String[] args) {
+        String s = "abcabcbb";
 
+        Solution sol = new Solution();
+        System.out.println(sol.lengthOfLongestSubstring(s));
     }
 }
